46cd1020a8c299741e283eeaaa3bf82b76dd4ffa
Maxwell Fernandes <maxwellfercar@gmail.com>
2025-05-13 20:43:15 -0300

    Add files via upload

---
+++ aula15/script.js
@@ -0,0 +1,8 @@
+/*document.getElementById("titulo").innerHTML = "OLÁ WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOW";
+document.getElementById("resposta").innerHTML = "O meu nome é " + window.prompt("Qual é o seu nome");*/
+/*function get_answer() {
+    document.getElementById("user_input").innerHTML = "O nome enviado foi: " + window.prompt("Digite seu nome: ");
+}*/
+function get_answer_2() {
+    window.alert(document.getElementById("label").value);
+}
